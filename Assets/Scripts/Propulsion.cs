1623d2a50d760b9a88c8550e8b8279f4e1bfba51
bdjewkes <bdjewkes@gmail.com>
2013-09-27 09:46:15 -0700

    initial commit

---
+++ Assets/Scripts/Propulsion.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using System.Collections;
+
+public class Propulsion : MonoBehaviour
+{
+      public bool active=false;
+      public float turnspeed = 5.0f;
+      public float rateofturn;
+      public float maxturnspeed = 10f;
+      public float thrust = 5.0f;
+      public float trueSpeed = 0.0f;
+      public float maxthrustahead=1f;
+      public float maxthrustastern=-0.6f;
+
+      void Update() // particlesystem behavior
+      {
+           ParticleSystem particlesystem = (ParticleSystem)gameObject.GetComponent("ParticleSystem");
+
+           if (Input.GetAxis("Power") > 0) { particlesystem.enableEmission = true; }
+           else { particlesystem.enableEmission = false; }
+      }
+    
+    void FixedUpdate()
+    {
+        if (active)
+        {
+            float yaw = Input.GetAxis("Yaw");
+            float power = Input.GetAxis("Power");
+            
+       
+           //Truespeed controls
+            if (trueSpeed < maxthrustahead && trueSpeed > maxthrustastern)
+            {
+             trueSpeed += thrust*power;
+            }
+            if (trueSpeed > maxthrustahead)
+            {
+                trueSpeed = maxthrustahead-0.1f;
+            }
+            if (trueSpeed < maxthrustastern)
+            {
+                trueSpeed = maxthrustastern+0.1f;
+            }
+            if (Input.GetKey("backspace"))
+            {
+                trueSpeed = 0;
+                rateofturn = 0;
+
+            }
+
+            if (power != 0)
+            {
+                rigidbody.AddRelativeForce(0, 0, trueSpeed * thrust * Time.deltaTime);
+            }
+            if (yaw != 0)
+            {
+                transform.Rotate(0, turnspeed * yaw * Time.deltaTime, 0);
+            }
+        }   
+    }
+}
+
+/*  Logic for rotation with momentum
+ * 
+ * 
+ *   rateofturn += yaw * turnspeed * Time.deltaTime;
+         rigidbody.AddRelativeTorque(0, yaw * turnspeed * Time.deltaTime, 0);
+         if ( rateofturn >= maxturnspeed)
+         {
+             rateofturn = (maxturnspeed - 0.1f);
+             rigidbody.AddRelativeTorque(0, yaw * turnspeed * -1.0f * Time.deltaTime, 0);
+         }
+         if (rateofturn <= maxturnspeed * -1)
+         {
+              rateofturn = (maxturnspeed * -1 + 0.1f);
+              rigidbody.AddRelativeTorque(0, yaw * turnspeed * -1.0f * Time.deltaTime, 0);
+
+          }
+ * 
+ * 
+*/
