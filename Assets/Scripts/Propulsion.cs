b55590eddeea954c999b098f604a0f4db22d1fb6
mjewkes <mjewkes@gmail.com>
2013-09-30 18:07:59 -0300

    propulsion changes

--- Assets/Scripts/Propulsion.cs
+++ Assets/Scripts/Propulsion.cs
@@ -34,23 +34,3 @@ public class Propulsion : MonoBehaviour
         rigidbody.velocity = currentVelocity;
     }
 }
-
-/*  Logic for rotation with momentum
- * 
- * 
- *   rateofturn += yaw * turnspeed * Time.deltaTime;
-         rigidbody.AddRelativeTorque(0, yaw * turnspeed * Time.deltaTime, 0);
-         if ( rateofturn >= maxturnspeed)
-         {
-             rateofturn = (maxturnspeed - 0.1f);
-             rigidbody.AddRelativeTorque(0, yaw * turnspeed * -1.0f * Time.deltaTime, 0);
-         }
-         if (rateofturn <= maxturnspeed * -1)
-         {
-              rateofturn = (maxturnspeed * -1 + 0.1f);
-              rigidbody.AddRelativeTorque(0, yaw * turnspeed * -1.0f * Time.deltaTime, 0);
-
-          }
- * 
- * 
-*/
