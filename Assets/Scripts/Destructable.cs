4d0008289572f0427a819e049b856345d05266b2
bdjewkes <bdjewkes@gmail.com>
2013-10-10 08:37:13 -0700

    Some variables added to destructables for upcoming rework

--- Assets/Scripts/Destructable.cs
+++ Assets/Scripts/Destructable.cs
@@ -3,15 +3,17 @@ using System.Collections;
 
 public class Destructable : MonoBehaviour {
     public bool destruct;
+    public float hull = 10; // hitpoints 
+    public float armor = 1; // damage reduction
     public GameObject explosion;
 
 	// Use this for initialization
 	void Start () {
-	
 	}
-	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 }
+
+
+    
